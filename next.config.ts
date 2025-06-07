dc9d87cb5a4eea1f6ab130ee8a8eb0932793395e
jennifertrin <jennifer.tran.2017@trincoll.edu>
2025-06-06 22:22:03 -0700

    fix: enable external image urls

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['imagedelivery.net'],
+    // OR use the newer remotePatterns (recommended):
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
+module.exports = nextConfig
 export default nextConfig;
