23c06895d5120845de918fad3402a27a05fed995
jennifertrin <jennifer.trangis@gmail.com>
2025-06-27 18:26:19 -0700

    chore: add QueryProvider

--- src/components/WagmiProvider.tsx
+++ src/components/WagmiProvider.tsx
@@ -1,8 +1,16 @@
 'use client'
 
 import { WagmiProvider as Provider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { config } from '../lib/wagmi'
 
+// Create a client
+const queryClient = new QueryClient()
+
 export function WagmiProvider({ children }: { children: React.ReactNode }) {
-  return <Provider config={config}>{children}</Provider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider config={config}>{children}</Provider>
+    </QueryClientProvider>
+  )
 } 
